ef7c1a9a5b1dc5967d38570e46dc30c9418ed9db
JoseA <lmj@iesseveroochoa.net>
2016-09-25 21:18:22 +0200

    desde lmj

--- app/src/main/java/es/iesseveroochoa/jlm/controlversiones/MainActivity.java
+++ app/src/main/java/es/iesseveroochoa/jlm/controlversiones/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //esto es una prueba de control de versiones commit 1
+        //esto es otra prueba de control de versiones commint 2
     }
 }
